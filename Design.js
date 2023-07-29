1691f80b8e4e428e40b4c44e26772c7ef8c7e0c1
Doctor Pranathi <126224688+Pranathi0000@users.noreply.github.com>
2023-07-29 10:13:21 +0530

    Add files via upload

---
+++ Design.js
@@ -0,0 +1,60 @@
+ReactDOM.render(
+    <div>
+       <center> <img src='https://wallpapers.com/images/hd/illuminated-jersey-dhoni-7-tengc39w5kgj50fr.jpg'></img> </center>
+
+    <h2>Introduction:</h2>
+    <p>
+        Mahendra Singh Dhoni, affectionately known as MS Dhoni, is an iconic Indian cricketer who revolutionized the sport with his unorthodox style and exceptional leadership skills.<br></br>
+        Born on July 7, 1981, in Ranchi, Jharkhand, India, Dhoni's journey from a small-town boy to becoming one of the most successful and admired cricketing legends is nothing short of inspirational.
+        <br></br>His calm demeanor under pressure earned him the nickname "Captain Cool," and he has left an indelible mark on Indian cricket.
+    </p>
+
+    
+    <h2>Rise in Domestic Cricket:</h2>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2cWusO-Lyt8MqeIz8OgU1ft2MnOagb4wXdg&usqp=CAU'></img>
+    <p>Dhoni's cricketing journey started with representing Bihar (before Jharkhand was formed) in domestic cricket. <br>
+    </br>His attacking batting style and ability to finish matches earned him recognition and caught the attention of selectors.<br>
+    </br> He showcased his skills in various domestic tournaments and gained valuable experience, setting the foundation for his future success in international cricket.</p>
+    
+    <h2>International Debut and Breakthrough:</h2>
+    <img src='https://cdn-wp.thesportsrush.com/2017/09/MANCHESTER-UNITED-TO-ATLETICO-DE-KOLKATA-42-1.png'></img>
+    <p>In 2004, Dhoni's hard work and talent paid off when he was selected to join the Indian cricket team for the Bangladesh tour.<br>
+    </br> He made his debut in international cricket and immediately made an impact with his explosive batting.<br>
+    </br> However, it was during a One Day International (ODI) against Pakistan in Visakhapatnam later that year when he truly announced his arrival.<br>
+    </br> Dhoni's breathtaking knock of 148 runs showcased his power-hitting abilities and marked the beginning of a memorable international career.</p>
+    
+    <h2>Captaincy and Leadership:</h2>
+    <img src='https://e1.pxfuel.com/desktop-wallpaper/990/213/desktop-wallpaper-%E2%9C%85-8-ms-dhoni-7-looking-at-sky-drawing-ms-dhoni-7-thumbnail.jpg'></img>
+    <p>Dhoni's leadership qualities came to the fore when he was appointed as the captain of the Indian cricket team for the inaugural ICC World Twenty20 held in South Africa in 2007.<br>
+    </br> Under his captaincy, the young Indian team defied expectations and won the tournament, putting India on the world map for T20 cricket.<br>
+    </br> Dhoni's calm demeanor, astute cricketing mind, and ability to remain composed under pressure earned him the nickname "Captain Cool."</p>
+    
+    <h2>The Pinnacle: ICC Cricket World Cup 2011:</h2>
+    <img src='https://static.toiimg.com/thumb/msid-81866476,width-1280,resizemode-4/81866476.jpg'></img>
+    <p>The crowning moment of Dhoni's career came on April 2, 2011, when he led India to a historic victory in the ICC Cricket World Cup held in India.<br>
+    </br> The entire nation was captivated as Dhoni played a captain's innings, scoring an unbeaten 91 runs in the final against Sri Lanka.<br>
+    </br> His calm and calculated approach in the high-stakes match, and the unforgettable six to seal the win, made him a national hero and an icon for generations to come.</p>
+    
+    <h2>Chennai Super Kings and IPL Success:</h2>
+    <img src='https://www.hindustantimes.com/ht-img/img/2023/05/20/1600x900/Dhoni-fans-in-Delhi-are-ready-with-their-yellow-je_1684580690837.jpg'></img>
+    <p>Dhoni's association with the Chennai Super Kings (CSK) franchise in the Indian Premier League (IPL) became legendary.<br>
+    </br> As the captain of CSK, he led the team to multiple IPL titles and established them as one of the most successful franchises in the tournament's history. <br>
+    </br>Dhoni's leadership, combined with his tactical acumen and calmness, made CSK a formidable force in the IPL.</p>
+    
+    <h2>Retirement from International Cricket:</h2>
+    <p>In August 2020, MS Dhoni announced his retirement from international cricket, marking the end of an illustrious era in Indian cricket.<br>
+    </br> Though he bid farewell to international matches, he continued to play in the IPL for a few more seasons before eventually retiring from all forms of cricket.</p>
+    
+    <h2>Legacy and Impact:</h2>
+    <img src='https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2021/09/14073840/ms-dhoni-1625654692.jpg'></img>
+    <p>MS Dhoni's impact on Indian cricket and the sport as a whole is immeasurable. <br>
+    </br>His journey from a small-town boy to a global cricketing icon serves as an inspiration to millions of aspiring athletes around the world. <br>
+    </br>Beyond his cricketing achievements, Dhoni is admired for his humility, integrity, and philanthropic work.</p>
+    
+    <h2>Beyond Cricket: Life After Retirement:</h2>
+    <p>After retiring from cricket, Dhoni remained involved in the game through business ventures and cricket administration.<br>
+    </br> He also used his platform to contribute to various social causes and charity work, particularly in support of underprivileged children and sports development in India.</p>
+
+    <img src='https://w0.peakpx.com/wallpaper/689/917/HD-wallpaper-ms-dhoni-chennai-super-kings-cricket-csk-definitely-not-india-sports.jpg'></img>
+</div>,
+document.getElementById('content'));
